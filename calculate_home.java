98874cf60aa2fbf9f76714e84fbd31d5a97dfa63
LikeaTruck <sanya.spb2@mail.ru>
2017-03-16 21:58:26 +0300

    calculate_home

---
+++ calculate_home.java
@@ -0,0 +1,18 @@
+public class calculate_home {
+public static void main (String[] args) {
+int a = 1000000;
+short b = 10;
+long c = 10l;
+boolean d = true;
+double e = 1.01;
+float f = 10.10f;
+String g = "Hello World!";
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+}
+}
